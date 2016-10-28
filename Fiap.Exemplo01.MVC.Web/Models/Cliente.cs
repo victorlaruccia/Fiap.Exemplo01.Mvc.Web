f18cdaeb62db0ba0ac806d12e33e82b3cd78257e
Victor <victorlaruccia10@outlook.com>
2016-10-28 18:36:18 -0200

    Adicionados necessidades especiais e data de nascimento

--- Fiap.Exemplo01.MVC.Web/Models/Cliente.cs
+++ Fiap.Exemplo01.MVC.Web/Models/Cliente.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -10,5 +11,10 @@ namespace Fiap.Exemplo01.MVC.Web.Models
         public string Nome { get; set; }
         public int Idade { get; set; }
         public decimal Credito { get; set; }
+        [Display(Name = "Possui necessidades especiais?")]
+        public bool NecessidadesEspeciais { get; set; }
+        [Display(Name = "Data de Nascimento")]
+        public DateTime DataNascimento { get; set; }
+        public string EstadoCivil { get; set; }
     }
 }
